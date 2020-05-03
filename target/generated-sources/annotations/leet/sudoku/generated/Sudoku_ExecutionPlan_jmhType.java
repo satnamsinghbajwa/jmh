7b7864020105d947d804356b23660d57b1fb0069
satnamsinghbajwa <satnam.singh.bajwa@gmail.com>
2020-05-03 14:40:16 -0700

    removed ignored files

--- target/generated-sources/annotations/leet/sudoku/generated/Sudoku_ExecutionPlan_jmhType.java
+++
@@ -1,4 +0,0 @@
-package leet.sudoku.generated;
-public class Sudoku_ExecutionPlan_jmhType extends Sudoku_ExecutionPlan_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/leet/sudoku/generated/Sudoku_jmhType.java
+++
@@ -1,4 +0,0 @@
-package leet.sudoku.generated;
-public class Sudoku_jmhType extends Sudoku_jmhType_B3 {
-}
-
